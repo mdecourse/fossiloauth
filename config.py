8d9a5403cfb726ba61df9925e061ba6c3ee4f007
mdecourse <mdecourse@gmail.com>
2021-06-26 21:30:11 +0800

    complete the very first version to allow user login with gmail account to Fossil SCM

--- config.py
+++ config.py
@@ -5,12 +5,7 @@ site = ["fossil.kmol.info", "8443"]
 domain_name = site[0]
 port = site[1]
 CALLBACK_URL = "https://"+domain_name+":"+port
-
-# read Fossil SCM repository username and password for testing
-with open('./../fossil_user.txt', 'r') as f:
-    fossil = f.read().splitlines()
-# user_name = fossil[0]
-# password = fossil[1]
+repo_path = "/home/yen/repository/u/yen.fossil"
 
 # read client_id and client_secret from safe place other than put into script
 # current setting only allow @gm user login
